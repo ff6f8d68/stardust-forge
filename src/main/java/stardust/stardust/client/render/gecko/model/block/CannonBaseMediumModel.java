224558d45c72a3ce52d84bc353560ccd2411227e
EnderTheCoder <ggameinvader@gmail.com>
2022-08-17 19:57:14 +0800

    rotate cannon base medium model to right direction and add useless mixin for block render fix

--- src/main/java/stardust/stardust/client/render/gecko/model/block/CannonBaseMediumModel.java
+++ src/main/java/stardust/stardust/client/render/gecko/model/block/CannonBaseMediumModel.java
@@ -4,6 +4,7 @@ import net.minecraft.util.ResourceLocation;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import stardust.stardust.Stardust;
 import stardust.stardust.entity.base.CannonBaseMediumTileEntity;
+import stardust.stardust.entity.cannon.medium.AbstractCannonMediumTileEntity;
 
 public class CannonBaseMediumModel extends AnimatedGeoModel<CannonBaseMediumTileEntity> {
     @Override
@@ -19,5 +20,12 @@ public class CannonBaseMediumModel extends AnimatedGeoModel<CannonBaseMediumTile
     @Override
     public ResourceLocation getAnimationFileLocation(CannonBaseMediumTileEntity animatable) {
         return null;
+    }
+
+    @Override
+    public void setLivingAnimations(CannonBaseMediumTileEntity entity, Integer uniqueID) {
+        super.setLivingAnimations(entity, uniqueID);
+        this.getAnimationProcessor().getBone("bone").setRotationX(-(float) Math.PI / 2);
+//        this.getAnimationProcessor().getBone("barrel_moving_up_down").setRotationZ((float) Math.toRadians(entity.nowRotationX));
     }
 }
