5445d5a689afe58b3f3c0870cb43d98fe2ad2372
EnderTheCoder <ggameinvader@gmail.com>
2022-11-08 21:46:06 +0800

    Make a better abstract structure for weapon blocks.

---
+++ src/main/java/stardust/stardust/turret/RotatableTurret.java
@@ -0,0 +1,44 @@
+package stardust.stardust.turret;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.item.Item;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.renderers.geo.GeoBlockRenderer;
+
+abstract public class RotatableTurret {
+    abstract public class Block extends net.minecraft.block.Block {
+
+        public Block(Properties properties) {
+            super(properties);
+        }
+    }
+
+    abstract public class Model extends AnimatedGeoModel <Tile>{
+
+    }
+
+    abstract public class Tile extends TileEntity implements IAnimatable {
+
+        public Tile(TileEntityType<?> tile) {
+            super(tile);
+        }
+    }
+
+    abstract public class Renderer extends GeoBlockRenderer<Tile> {
+
+        public Renderer(TileEntityRendererDispatcher rendererDispatcherIn, AnimatedGeoModel<Tile> modelProvider) {
+            super(rendererDispatcherIn, modelProvider);
+        }
+    }
+
+    abstract public class Item extends net.minecraft.item.Item {
+
+        public Item(Properties properties) {
+            super(properties);
+        }
+    }
+}
