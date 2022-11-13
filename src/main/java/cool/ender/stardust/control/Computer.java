ffa2fc7639633b2d50c26190466f76196eed6585
EnderTheCoder <ggameinvader@gmail.com>
2022-11-13 16:53:50 +0800

    Fix(Computer): Fix computer rendering.

--- src/main/java/cool/ender/stardust/control/Computer.java
+++ src/main/java/cool/ender/stardust/control/Computer.java
@@ -9,6 +9,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.RenderShape;
@@ -17,13 +18,14 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -58,7 +60,20 @@ public class Computer {
                 return InteractionResult.CONSUME;
             }
         }
+        @Override
+        public int getLightBlock(BlockState p_60585_, BlockGetter p_60586_, BlockPos p_60587_) {
+            return 15;
+        }
 
+        @Override
+        public int getLightEmission(BlockState state, BlockGetter level, BlockPos pos) {
+            return 15;
+        }
+
+        @Override
+        public VoxelShape getShape(BlockState p_52807_, BlockGetter p_52808_, BlockPos p_52809_, CollisionContext p_52810_) {
+            return box(0.0D, 0.0D, 2.0D, 16.0D, 16.0D, 16.0D);
+        }
 
         @Nullable
         @Override
@@ -71,29 +86,45 @@ public class Computer {
         public AnimationFactory factory = GeckoLibUtil.createFactory(this);
 
         boolean open = false;
+        boolean isProgressing = false;
+
         public Tile(BlockEntityType<?> p_155228_, BlockPos p_155229_, BlockState p_155230_) {
             super(p_155228_, p_155229_, p_155230_);
         }
+
         public Tile(BlockPos p_155229_, BlockState p_155230_) {
             this(TileRegistry.COMPUTER_TILE.get(), p_155229_, p_155230_);
         }
+
         @Override
         public void registerControllers(AnimationData data) {
-            data.addAnimationController(new AnimationController(this, "controller", 0, this::predicate));
+            data.addAnimationController(new AnimationController(this, "controller", 20, this::predicate));
+        }
+
+        public void open() {
+            this.isProgressing = true;
+        }
+
+        public void close() {
+
         }
 
         private <E extends BlockEntity & IAnimatable> PlayState predicate(AnimationEvent<E> event) {
             AnimationController<E> controller = event.getController();
-            controller.transitionLengthTicks = 0;
-            if (open) {
-                if (controller.getAnimationState() == AnimationState.Stopped) {
-                    controller.setAnimation(new AnimationBuilder().addAnimation("open", ILoopType.EDefaultLoopTypes.HOLD_ON_LAST_FRAME));
-                    return PlayState.CONTINUE;
-                }
-            } else {
-                if (controller.getAnimationState() == AnimationState.Stopped) {
+            controller.transitionLengthTicks = 20;
+            if (isProgressing) {
+                Stardust.LOGGER.info("progressing");
+                if (open) {
                     controller.setAnimation(new AnimationBuilder().addAnimation("close", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-                    return PlayState.CONTINUE;
+                } else {
+                    controller.setAnimation(new AnimationBuilder().addAnimation("open", ILoopType.EDefaultLoopTypes.HOLD_ON_LAST_FRAME));
+                }
+                this.open = !this.open;
+                this.isProgressing = false;
+            } else {
+                if (this.open) {
+                    controller.setAnimation(new AnimationBuilder().addAnimation("keep", ILoopType.EDefaultLoopTypes.LOOP));
+                    return PlayState.STOP;
                 }
             }
             return PlayState.CONTINUE;
--- src/main/java/cool/ender/stardust/registry/ItemRegistry.java
+++ src/main/java/cool/ender/stardust/registry/ItemRegistry.java
@@ -13,5 +13,7 @@ import static cool.ender.stardust.turret.AbstractTurret.REGISTRY_TYPE.ITEM;
 
 public class ItemRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Stardust.MOD_ID);
-    public static final RegistryObject<BlockItem> RAIL_GUN_1_SMALL = ITEMS.register(new RailGun1Small().getRegisterName(ITEM), () -> new BlockItem(BlockRegistry.RAIL_GUN_1_SMALL_BLOCK.get(), new Item.Properties().tab(GeckoLibMod.geckolibItemGroup)));
+    public static final RegistryObject<BlockItem> RAIL_GUN_1_SMALL_ITEM = ITEMS.register(new RailGun1Small().getRegisterName(ITEM), () -> new BlockItem(BlockRegistry.RAIL_GUN_1_SMALL_BLOCK.get(), new Item.Properties().tab(GeckoLibMod.geckolibItemGroup)));
+    public static final RegistryObject<BlockItem> COMPUTER_ITEM = ITEMS.register("computer_item", () -> new BlockItem(BlockRegistry.COMPUTER_BLOCK.get(), new Item.Properties().tab(GeckoLibMod.geckolibItemGroup)));
+
 }
