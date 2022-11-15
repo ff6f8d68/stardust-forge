d8e6bca441b2d3f14fbcb02709a0005503a32240
EnderTheCoder <ggameinvader@gmail.com>
2022-11-15 22:14:27 +0800

    Chore(Tube): add resource files.

--- src/main/java/cool/ender/stardust/registry/ItemRegistry.java
+++ src/main/java/cool/ender/stardust/registry/ItemRegistry.java
@@ -1,6 +1,7 @@
 package cool.ender.stardust.registry;
 
 import cool.ender.stardust.Stardust;
+import cool.ender.stardust.tube.Tube;
 import cool.ender.stardust.turret.small.RailGun1Small;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -16,5 +17,6 @@ public class ItemRegistry {
     public static final RegistryObject<BlockItem> RAIL_GUN_1_SMALL_ITEM = ITEMS.register(new RailGun1Small().getRegisterName(ITEM), () -> new BlockItem(BlockRegistry.RAIL_GUN_1_SMALL_BLOCK.get(), new Item.Properties().tab(GeckoLibMod.geckolibItemGroup)));
     public static final RegistryObject<BlockItem> COMPUTER_ITEM = ITEMS.register("computer_item", () -> new BlockItem(BlockRegistry.COMPUTER_BLOCK.get(), new Item.Properties().tab(GeckoLibMod.geckolibItemGroup)));
     public static final RegistryObject<BlockItem> SHIELD_GENERATOR_ITEM = ITEMS.register("shield_generator_item", () -> new BlockItem(BlockRegistry.SHIELD_GENERATOR_BLOCK.get(), new Item.Properties().tab(GeckoLibMod.geckolibItemGroup)));
+    public static final RegistryObject<BlockItem> TUBE = ITEMS.register(new Tube().getRegisterName(),() -> new BlockItem(BlockRegistry.TUBE_BLOCK.get(), new Item.Properties().tab(GeckoLibMod.geckolibItemGroup)));
 
 }
