c6aad11c0c19cd1ee3b92350c7c1e4589a699889
EnderTheCoder <ggameinvader@gmail.com>
2022-12-07 23:53:51 +0800

    Feat(Missile): rotation fix

--- src/main/java/cool/ender/stardust/missile/Missile.java
+++ src/main/java/cool/ender/stardust/missile/Missile.java
@@ -8,10 +8,13 @@ import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.manager.AnimationData;
@@ -53,14 +56,47 @@ public class Missile {
 
         @Override
         public void tick() {
-            super.tick();
+
+            net.minecraft.world.entity.Entity entity = this.getOwner();
+            if (this.level.isClientSide || (entity == null || !entity.isRemoved()) && this.level.hasChunkAt(this.blockPosition())) {
+                super.tick();
+                if (this.shouldBurn()) {
+                    this.setSecondsOnFire(1);
+                }
+
+                HitResult hitresult = ProjectileUtil.getHitResult(this, this::canHitEntity);
+                if (hitresult.getType() != HitResult.Type.MISS && !net.minecraftforge.event.ForgeEventFactory.onProjectileImpact(this, hitresult)) {
+                    this.onHit(hitresult);
+                }
+
+                this.checkInsideBlocks();
+                Vec3 vec3 = this.getDeltaMovement();
+                double d0 = this.getX() + vec3.x;
+                double d1 = this.getY() + vec3.y;
+                double d2 = this.getZ() + vec3.z;
+                float f = this.getInertia();
+                if (this.isInWater()) {
+                    for(int i = 0; i < 4; ++i) {
+                        float f1 = 0.25F;
+                        this.level.addParticle(ParticleTypes.BUBBLE, d0 - vec3.x * 0.25D, d1 - vec3.y * 0.25D, d2 - vec3.z * 0.25D, vec3.x, vec3.y, vec3.z);
+                    }
+
+                    f = 0.8F;
+                }
+
+                this.setDeltaMovement(vec3.add(this.xPower, this.yPower, this.zPower).scale((double)f));
+                this.level.addParticle(this.getTrailParticle(), d0, d1 + 0.5D, d2, 0.0D, 0.0D, 0.0D);
+                this.setPos(d0, d1, d2);
+            } else {
+                this.discard();
+            }
+
             if (!this.level.isClientSide) {
                 this.age++;
                 if (this.age < 20) {
                     this.yPower = this.yPower - 0.05;
-                    Stardust.LOGGER.info(this.yPower);
                 } else if (this.age == 20) {
-                    this.yPower = 2;
+                    this.yPower = 0.5;
                 } else {
                     Vec3 vec0 = this.getEyePosition();
                     Vec3 vec1 = this.getTargetPos();
@@ -79,19 +115,11 @@ public class Missile {
             return new Vec3(0, 0, 0);
         }
 
-        private Vec3 getToTargetVec() {
-            return getTargetPos().subtract(this.position());
+        @Override
+        public float getXRot() {
+            return (float) (super.getXRot() - Math.PI / 2);
         }
 
-//        @Override
-//        public float getXRot() {
-//            return this.age < 20 ? 0 : super.getXRot();
-//        }
-//
-//        @Override
-//        public float getYRot() {
-//            return this.age < 20 ? 0 : super.getYRot();
-//        }
 
         @Override
         protected ParticleOptions getTrailParticle() {
