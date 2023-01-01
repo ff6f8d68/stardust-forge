579f832e1e43c3662c59f6815e8c45347fb00b03
ender <ggameinvader@gmail.com>
2023-01-02 02:23:40 +0800

    First gui with bg renderer.

---
+++ src/main/java/cool/ender/stardust/client/gui/AllGuiTextures.java
@@ -0,0 +1,64 @@
+package cool.ender.stardust.client.gui;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import cool.ender.stardust.Stardust;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public enum AllGuiTextures {
+
+    COMPUTER_GUI("computer_gui", 283, 102);
+
+    public static final int FONT_COLOR = 0x575F7A;
+
+    public final ResourceLocation location;
+    public int width, height;
+    public int startX, startY;
+
+    private AllGuiTextures(String location, int width, int height) {
+        this(location, 0, 0, width, height);
+    }
+
+    private AllGuiTextures(int startX, int startY) {
+        this("icons", startX * 16, startY * 16, 16, 16);
+    }
+
+    private AllGuiTextures(String location, int startX, int startY, int width, int height) {
+        this(Stardust.MOD_ID, location, startX, startY, width, height);
+    }
+
+    private AllGuiTextures(String namespace, String location, int startX, int startY, int width, int height) {
+        this.location = new ResourceLocation(namespace, "textures/gui/" + location + ".png");
+        this.width = width;
+        this.height = height;
+        this.startX = startX;
+        this.startY = startY;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public void bind() {
+        RenderSystem.setShaderTexture(0, location);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public void render(PoseStack ms, int x, int y) {
+        bind();
+        GuiComponent.blit(ms, x, y, 0, startX, startY, width, height, 256, 256);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public void render(PoseStack ms, int x, int y, GuiComponent component) {
+        bind();
+        component.blit(ms, x, y, startX, startY, width, height);
+    }
+
+//    @OnlyIn(Dist.CLIENT)
+//    public void render(PoseStack ms, int x, int y, Color c) {
+//        bind();
+//        UIRenderHelper.drawColoredTexture(ms, c, x, y, startX, startY, width, height);
+//    }
+
+}
--- src/main/java/cool/ender/stardust/client/gui/VerticalLauncherScreen.java
+++ src/main/java/cool/ender/stardust/client/gui/VerticalLauncherScreen.java
@@ -1,15 +1,20 @@
 package cool.ender.stardust.client.gui;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import cool.ender.stardust.missile.launcher.VerticalMissileLauncher;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.ProgressOption;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.components.SliderButton;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
@@ -29,7 +34,12 @@ public class VerticalLauncherScreen extends Screen {
 
     Button explodeOption;
 
+    int imageHeight = 256;
+    int imageWidth = 256;
+
     VerticalMissileLauncher.Tile bindedTile;
+
+    public static final ResourceLocation COMPUTER_GUI = new ResourceLocation("stardust", "textures/gui/computer_gui.png");
 
     public VerticalLauncherScreen(Component p_96550_, VerticalMissileLauncher.Tile tile) {
         super(p_96550_);
@@ -40,14 +50,15 @@ public class VerticalLauncherScreen extends Screen {
     protected void init() {
         assert this.minecraft != null;
         this.minecraft.keyboardHandler.setSendRepeatsToGui(true);
+        int topBaseHeight = (this.height - this.imageHeight) / 2;
         int coordinateInputOffsetX = 0;
-        this.coordinateX = new EditBox(this.font, this.width / 2 - 100 + coordinateInputOffsetX, 66, 60, 20, new TranslatableComponent("gui.stardust.coordinates_input.x"));
+        this.coordinateX = new EditBox(this.font, this.width / 2 - 100 + coordinateInputOffsetX, topBaseHeight + 40, 60, 20, new TranslatableComponent("gui.stardust.coordinates_input.x"));
         coordinateInputOffsetX += 70;
-        this.coordinateY = new EditBox(this.font, this.width / 2 - 100 + coordinateInputOffsetX, 66, 60, 20, new TranslatableComponent("gui.stardust.coordinates_input.y"));
+        this.coordinateY = new EditBox(this.font, this.width / 2 - 100 + coordinateInputOffsetX, topBaseHeight + 40, 60, 20, new TranslatableComponent("gui.stardust.coordinates_input.y"));
         coordinateInputOffsetX += 70;
-        this.coordinateZ = new EditBox(this.font, this.width / 2 - 100 + coordinateInputOffsetX, 66, 60, 20, new TranslatableComponent("gui.stardust.coordinates_input.z"));
+        this.coordinateZ = new EditBox(this.font, this.width / 2 - 100 + coordinateInputOffsetX, topBaseHeight + 40, 60, 20, new TranslatableComponent("gui.stardust.coordinates_input.z"));
 
-        this.controlMode = new Button(this.width / 2 - 100, 91, 200, 20, new TranslatableComponent("gui.stardust.vertical_launcher.control_mode").append(new TextComponent(":").append(Objects.requireNonNull(bindedTile.controlMode.getComponent()))), (button) -> {
+        this.controlMode = new Button(this.width / 2 - 100, topBaseHeight + 40 + 30, 200, 20, new TranslatableComponent("gui.stardust.vertical_launcher.control_mode").append(new TextComponent(":").append(Objects.requireNonNull(bindedTile.controlMode.getComponent()))), (button) -> {
             this.bindedTile.switchControlMode();
             if (bindedTile.controlMode != VerticalMissileLauncher.ControlMode.COORDINATE) {
                 coordinateX.setEditable(false);
@@ -60,11 +71,11 @@ public class VerticalLauncherScreen extends Screen {
             }
             button.setMessage(new TranslatableComponent("gui.stardust.vertical_launcher.control_mode").append(new TextComponent(":").append(Objects.requireNonNull(bindedTile.controlMode.getComponent()))));
         });
-        this.explodeOption = new Button(this.width / 2 - 100, 116, 200, 20, new TranslatableComponent("gui.stardust.vertical_launcher.explode_on_discard").append(new TextComponent(":").append(bindedTile.explodeOnDiscard ? new TranslatableComponent("message.stardust.yes") : new TranslatableComponent("message.stardust.no"))), (button) -> {
+        this.explodeOption = new Button(this.width / 2 - 100, topBaseHeight + 40 + 30 + 30, 200, 20, new TranslatableComponent("gui.stardust.vertical_launcher.explode_on_discard").append(new TextComponent(":").append(bindedTile.explodeOnDiscard ? new TranslatableComponent("message.stardust.yes") : new TranslatableComponent("message.stardust.no"))), (button) -> {
             this.bindedTile.switchExplodeOption();
             button.setMessage(new TranslatableComponent("gui.stardust.vertical_launcher.explode_on_discard").append(new TextComponent(":").append(bindedTile.explodeOnDiscard ? new TranslatableComponent("message.stardust.yes") : new TranslatableComponent("message.stardust.no"))));
         });
-        this.enterTelevision = new Button(this.width / 2 - 100, 66, 200, 20, new TranslatableComponent("gui.stardust.vertical_launcher.enter_television"), (button) -> {
+        this.enterTelevision = new Button(this.width / 2 - 100, topBaseHeight + 40, 200, 20, new TranslatableComponent("gui.stardust.vertical_launcher.enter_television"), (button) -> {
             this.onClose();
             // TODO: television code
         });
@@ -78,15 +89,24 @@ public class VerticalLauncherScreen extends Screen {
 
     @Override
     public void render(@NotNull PoseStack poseStack, int mouseX, int mouseY, float particleTick) {
+        this.renderBackground(poseStack);
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, COMPUTER_GUI);
+        int i = (this.width - this.imageWidth) / 2;
+        int j = (this.height - this.imageHeight) / 2;
+        this.blit(poseStack, i, j, 0, 0, this.imageWidth, this.imageHeight);
+
         if (this.bindedTile.controlMode == VerticalMissileLauncher.ControlMode.COORDINATE) {
-            drawString(poseStack, this.font, new TextComponent("X:"), this.width / 2 - 120 + 10, 71, 16777215);
-            drawString(poseStack, this.font, new TextComponent("Y:"), this.width / 2 - 50 + 10, 71, 16777215);
-            drawString(poseStack, this.font, new TextComponent("Z:"), this.width / 2 + 20 + 10, 71, 16777215);
+            drawString(poseStack, this.font, new TextComponent("X:"), this.width / 2 - 120 + 10, (this.height - this.imageHeight) / 2 + 45, 16777215);
+            drawString(poseStack, this.font, new TextComponent("Y:"), this.width / 2 - 50 + 10, (this.height - this.imageHeight) / 2 + 45, 16777215);
+            drawString(poseStack, this.font, new TextComponent("Z:"), this.width / 2 + 20 + 10, (this.height - this.imageHeight) / 2 + 45, 16777215);
 
             this.coordinateX.render(poseStack, mouseX, mouseY, particleTick);
             this.coordinateY.render(poseStack, mouseX, mouseY, particleTick);
             this.coordinateZ.render(poseStack, mouseX, mouseY, particleTick);
         }
+
 
         if (this.bindedTile.controlMode == VerticalMissileLauncher.ControlMode.TELEVISION) {
             this.enterTelevision.render(poseStack, mouseX, mouseY, particleTick);
