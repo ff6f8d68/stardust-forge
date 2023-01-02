f04ebb5dd52a374ac737b9497d0b3af92069a857
ender <ggameinvader@gmail.com>
2023-01-02 13:54:13 +0800

    add 2 interfaces for pipe based gui control system

---
+++ src/main/java/cool/ender/stardust/control/GuiProvider.java
@@ -0,0 +1,9 @@
+package cool.ender.stardust.control;
+
+import net.minecraft.client.gui.screens.Screen;
+
+import java.util.List;
+
+public interface GuiProvider {
+    List<Screen> getGuiList();
+}
---
+++ src/main/java/cool/ender/stardust/tube/TubeConnectable.java
@@ -0,0 +1,7 @@
+package cool.ender.stardust.tube;
+
+import net.minecraft.core.Direction;
+
+public interface TubeConnectable {
+    boolean getConnectable(Direction direction);
+}
